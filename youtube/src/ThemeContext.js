43600d64923792c7700475e25283144c7bbf28c3
bahetiNaman <nbaheti@rippling.com>
2022-07-12 11:46:37 +0530

    Revert "feature: basic youtube app with dark/light theme"
    This reverts commit c8a4f065a21279023175bb0e389b2b27425899d1.

--- youtube/src/ThemeContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import { themes } from "./assets/themes";
-
-
-// default state is dark
-// creating context for theme
-export const ThemeContext = createContext({theme: themes.dark, getTrendingVideos: () => {}, getVideosBasedOnSearch: () => {}, changeTheme: () => {},});
--- youtube/src/assets/colors.js
+++
@@ -1,7 +0,0 @@
-export const Colors = {
-  white: '#fff',
-  black: '#000',
-  grey: '#808080',
-  darkgray: '#a9a9a9',
-  darkslategray: '#2f4f4f',
-}
